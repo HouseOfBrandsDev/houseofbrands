6cd35d21ba80037d2df39e24f0d28e1db96e2f2b
HouseOfBrandsDev <contact@houseofbrands.fr>
2025-07-03 19:00:48 +0200

    Réparation de index.php : boucle principale et inclusion du footer

--- index.php
+++ index.php
@@ -6,22 +6,25 @@
  */
 
 get_header();
+?>
 
-if ( have_posts() ) :
-    while ( have_posts() ) :
-        the_post();
-        get_template_part( 'template-parts/content/content', get_post_type() );
-    endwhile;
+<main class="site-main container">
+  <?php if ( have_posts() ) : ?>
+    <?php while ( have_posts() ) : the_post(); ?>
+      <?php get_template_part( 'template-parts/content/content' ); ?>
+    <?php endwhile; ?>
 
-    the_posts_pagination(
-        array(
-            'mid_size'  => 2,
-            'prev_text' => esc_html__( '« Précédent', 'houseofbrands' ),
-            'next_text' => esc_html__( 'Suivant »', 'houseofbrands' ),
-        )
-    );
-else :
-    get_template_part( 'template-parts/content/content', 'none' );
-endif;
+    <?php the_posts_pagination(
+      array(
+        'mid_size'  => 2,
+        'prev_text' => esc_html__( '« Précédent', 'houseofbrands' ),
+        'next_text' => esc_html__( 'Suivant »', 'houseofbrands' ),
+      )
+    ); ?>
+  <?php else : ?>
+    <?php get_template_part( 'template-parts/content/content', 'none' ); ?>
+  <?php endif; ?>
+</main>
 
+<?php
 get_footer();
