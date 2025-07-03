49f968a59c75b024feabf029fbd3dc8fc3a45f42
HouseOfBrandsDev <contact@houseofbrands.fr>
2025-07-03 18:56:32 +0200

    Ajout de la boucle principale dans index.php

--- index.php
+++ index.php
@@ -1,31 +1,27 @@
 <?php
 /**
  * The main template file
+ *
  * @package HouseOfBrands
  */
 
 get_header();
 
 if ( have_posts() ) :
-  while ( have_posts() ) :
-    the_post();
-    // Utilise notre template-parts/content.php
-    get_template_part( 'template-parts/content/content', get_post_type() );
-  endwhile;
+    while ( have_posts() ) :
+        the_post();
+        get_template_part( 'template-parts/content/content', get_post_type() );
+    endwhile;
 
-  // Pagination
-  the_posts_pagination(
-    array(
-      'mid_size'  => 2,
-      'prev_text' => esc_html__( '« Précédent', 'houseofbrands' ),
-      'next_text' => esc_html__( 'Suivant »', 'houseofbrands' ),
-    )
-  );
-
+    the_posts_pagination(
+        array(
+            'mid_size'  => 2,
+            'prev_text' => esc_html__( '« Précédent', 'houseofbrands' ),
+            'next_text' => esc_html__( 'Suivant »', 'houseofbrands' ),
+        )
+    );
 else :
-  // Aucun contenu trouvé
-  get_template_part( 'template-parts/content/content', 'none' );
-
+    get_template_part( 'template-parts/content/content', 'none' );
 endif;
 
 get_footer();
