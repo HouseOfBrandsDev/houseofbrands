76e16362a2b66ee4f618765d052eb711409e6e4e
HouseOfBrandsDev <contact@houseofbrands.fr>
2025-07-03 18:53:25 +0200

    Ajout des templates de base (index, page, single, archive, search, 404)

--- index.php
+++ index.php
@@ -1,57 +1,31 @@
 <?php
 /**
  * The main template file
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package _s
+ * @package HouseOfBrands
  */
 
 get_header();
-?>
 
-	<main id="primary" class="site-main">
+if ( have_posts() ) :
+  while ( have_posts() ) :
+    the_post();
+    // Utilise notre template-parts/content.php
+    get_template_part( 'template-parts/content/content', get_post_type() );
+  endwhile;
 
-		<?php
-		if ( have_posts() ) :
+  // Pagination
+  the_posts_pagination(
+    array(
+      'mid_size'  => 2,
+      'prev_text' => esc_html__( '« Précédent', 'houseofbrands' ),
+      'next_text' => esc_html__( 'Suivant »', 'houseofbrands' ),
+    )
+  );
 
-			if ( is_home() && ! is_front_page() ) :
-				?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-				<?php
-			endif;
+else :
+  // Aucun contenu trouvé
+  get_template_part( 'template-parts/content/content', 'none' );
 
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+endif;
 
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
-
-	</main><!-- #main -->
-
-<?php
-get_sidebar();
 get_footer();
