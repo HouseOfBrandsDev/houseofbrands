1e2ef4f3f1ffca7f2bdd176e29fb93d5c174949f
HouseOfBrandsDev <contact@houseofbrands.fr>
2025-07-03 18:12:34 +0200

    Changement header

--- header.php
+++ header.php
@@ -9,31 +9,6 @@
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 
-<header class="site-header">
-  <div class="container u-text-center">
-    <?php if ( has_custom_logo() ) : ?>
-      <div class="site-logo">
-        <?php the_custom_logo(); ?>
-      </div>
-    <?php else : ?>
-      <h1 class="site-logo">
-        <a href="<?php echo esc_url( home_url( '/' ) ); ?>">
-          <?php bloginfo( 'name' ); ?>
-        </a>
-      </h1>
-    <?php endif; ?>
-
-    <nav class="site-navigation" role="navigation" aria-label="Menu principal">
-      <?php
-        wp_nav_menu( array(
-          'theme_location' => 'primary',
-          'menu_class'     => 'menu',
-          'container'      => false,
-          'fallback_cb'    => false
-        ) );
-      ?>
-    </nav>
-  </div>
-</header>
+<?php get_template_part( 'template-parts/header/site', 'header' ); ?>
 
 <main class="site-main container">
